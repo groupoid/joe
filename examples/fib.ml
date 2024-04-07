706f59fb19aab808460127d1b84061952fc6ed90
5HT <maxim@synrc.com>
2024-04-07 07:43:13 +0300

    fib

--- examples/fib.ml
+++ examples/fib.ml
@@ -1,1 +1,1 @@
-let rec fib n = if n <= 1 then 1 else fib(n-1) + fib(n-2) in let a = fib 4 in print_int(a);print_newline()
+let rec fib n = if n <= 1 then 1 else fib(n-1) + fib(n-2) in print_int(fib (read_int()));print_newline()
