2bdd069b11a9e9099b55af5e0ab14d423a5d9e5a
5HT <maxim@synrc.com>
2025-04-21 19:59:37 +0300

    wip

--- src/ulc/ulc.ml
+++ src/ulc/ulc.ml
@@ -2,16 +2,28 @@ type term =
   | Var of string
   | Lam of string * term
   | App of term * term
+  | Pair of term * term
+  | Fst of term
+  | Snd of term
+  | Unit
 
 let rec string_of_term = function
   | Var x -> x
   | Lam (x, t) -> "λ" ^ x ^ ". " ^ string_of_term t
   | App (t1, t2) -> "(" ^ string_of_term t1 ^ " " ^ string_of_term t2 ^ ")"
+  | Pair (t1, t2) -> "<" ^ string_of_term t1 ^ ", " ^ string_of_term t2 ^ ">"
+  | Fst t -> "fst " ^ string_of_term t
+  | Snd t -> "snd " ^ string_of_term t
+  | Unit -> "unit"
 
 let rec subst x s = function
   | Var y -> if x = y then s else Var y
   | Lam (y, t) when x <> y -> Lam (y, subst x s t)
   | App (f, a) -> App (subst x s f, subst x s a)
+  | Pair (t1, t2) -> Pair (subst x s t1, subst x s t2)
+  | Fst t -> Fst (subst x s t)
+  | Snd t -> Snd (subst x s t)
+  | Unit -> Unit
   | t -> t
 
 let rec equal t1 t2 =
@@ -21,11 +33,21 @@ let rec equal t1 t2 =
   | Lam (x, b), t -> equal b (App (t, Var x))
   | t, Lam (x, b) -> equal (App (t, Var x)) b
   | App (f1, a1), App (f2, a2) -> equal f1 f2 && equal a1 a2
+  | Pair (t1, t2), Pair (t1', t2') -> equal t1 t1' && equal t2 t2'
+  | Fst t, Fst t' -> equal t t'
+  | Snd t, Snd t' -> equal t t'
+  | Unit, Unit -> true
   | _ -> false
 
 let rec reduce = function
   | App (Lam (x, b), a) -> subst x a b
   | App (f, a) -> App (reduce f, reduce a)
+  | Pair (t1, t2) -> Pair (reduce t1, reduce t2)
+  | Fst (Pair (t1, t2)) -> t1
+  | Fst t -> Fst (reduce t)
+  | Snd (Pair (t1, t2)) -> t2
+  | Snd t -> Snd (reduce t)
+  | Unit -> Unit
   | t -> t
 
 let rec normalize t =
@@ -37,11 +59,14 @@ let const = Lam ("x", Lam ("y", Var "x"))
 let one = Lam ("f", Lam ("x", App (Var "f", Var "x")))
 let two = Lam ("f", Lam ("x", App (Var "f", App (Var "f", Var "x"))))
 
-let beta           = (App (Lam ("x", Var "x"), Var "y"), Var "y")
-let eta            = (Lam ("x", App (Var "f", Var "x")), Var "f")
-let eta_domain     = (Lam ("x", App (Var "f", Var "x")), Var "f")
-let invalid_eta    = (Lam ("x", Var "z"), Var "z")
-let invalid_eta_v  = (Lam ("x", Var "u"), Var "u")
+let beta = (App (Lam ("x", Var "x"), Var "y"), Var "y")
+let eta = (Lam ("x", App (Var "f", Var "x")), Var "f")
+let eta_domain = (Lam ("x", App (Var "f", Var "x")), Var "f")
+let invalid_eta = (Lam ("x", Var "z"), Var "z")
+let invalid_eta_v = (Lam ("x", Var "u"), Var "u")
+let pair_test = (Fst (Pair (Var "x", Var "y")), Var "x")
+let snd_test = (Snd (Pair (Var "x", Var "y")), Var "y")
+let unit_test = (App (Lam ("x", Unit), Var "z"), Unit)
 
 let test_equal name (t1, t2) =
   let t1' = normalize t1 in
@@ -59,3 +84,6 @@ let () =
   test_equal "Eta Domain" eta_domain;
   test_equal "Invalid Eta" invalid_eta;
   test_equal "Invalid Eta Var" invalid_eta_v;
+  test_equal "Pair Fst" pair_test;
+  test_equal "Pair Snd" snd_test;
+  test_equal "Unit" unit_test;
